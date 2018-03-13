adcc4929eb4acce3dd1ac7c7aa55018ef1a5ac8b
Corey Newhouse <corey.newhouse@deutschinc.com>
2018-03-12 23:12:59 -0700

    add data

--- server/data.js
+++ server/data.js
@@ -1,32 +1,40 @@
 const fs = require('fs')
-const json = require('./trailers.json')
+const json = require('./data.json')
 const _ = require('lodash')
+const ObjectId = require('mongodb').ObjectId
 
 const data = json.filter(trailer => trailer.Type)
 const music = []
 const trailers = []
 
 data.forEach(trailer => {
+  const id = trailer.Link.match(/^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/)
   const obj =  {
-    _id: trailer.Title,
+    _id: new ObjectId(),
     title: trailer.Title,
     type: trailer.Type,
     year: trailer.Year,
-    videoUrl: trailer.Link
+    videoUrl: trailer.Link,
+  }
+
+  if (id) {
+    obj.youtubeId = id[1]
   }
 
   const cues = ['C1', 'C2', 'C3'].map(cue => ({
+      song: new ObjectId(),
       artist: trailer[`${cue}_artist`],
       time: trailer[`${cue}_time`],
       title: trailer[`${cue}_title`]
     })
-  )
+  ).filter(cue => cue.artist)
 
   trailers.push({ ...obj, cues })
-  music.push(...cues.filter(cue => cue.artist))
+  music.push(...cues.map(cue => ({ _id: cue.song, artist: cue.artist, title: cue.title })))
 })
 
 // console.log(music)
 // console.log(trailers)
 
-fs.writeFile('music.json', JSON.stringify(_.orderBy(music, ['artist'])), () => console.log('done'))
+// fs.writeFile('music.json', JSON.stringify(_.orderBy(music, ['artist'])), () => console.log('music done'))
+fs.writeFile('trailers.json', JSON.stringify(_.orderBy(trailers, ['year', 'title', 'type'], ['desc'])), () => console.log('trailers done'))
